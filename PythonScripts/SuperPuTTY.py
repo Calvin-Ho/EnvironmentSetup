01a4bae3333e8bb8bed783bcc639216a9cb4c5d8
Calvin Ho <CalvinHoEmail@gmail.com>
2018-08-27 11:45:01 -0700

    Adding ability to change settings on all sessions
    Cleaned up some useless variables and folded session_selection into
    open_session function

--- PythonScripts/SuperPuTTY.py
+++ PythonScripts/SuperPuTTY.py
@@ -7,18 +7,19 @@ import time
 SESSION_COUNT = 6
 
 # Hard-coded coordinates for interactive program elements
+# -------------------------------------------------------------------------------
 # (Not really any way to make this better currently # as pyautogui has no support
 # for in-window references [8/20/18])
 host_field_coords = (330, 73)
 open_sess_check_coords = (1038, 74)
-clear_fields_coords = (1064, 74)
+clr_fields_button_coords = (1064, 74)
+first_sess_tab_coords = (89, 137)
+change_settings_menu_item_coords = (200, 340)
 
-# Hard-coded offsets for session tabs
-# --------------------------------------
-# Distance from center of one session tab to next tab
-sess_tab_offset = (110, 140)
-# Vertical distance from tab to actual session
-sess_tab_vert_offset = 100
+# Store these coordinates for clicking into session window to type password
+sess_window_coords = (110, 240)
+
+settings_change_x_offset = 136
 
 # SENSITIVE LOGIN INFORMATION
 '''
@@ -35,46 +36,55 @@ def main():
     time.sleep(3)
 
     open_session(SESSION_COUNT)
+    apply_settings(SESSION_COUNT)
 
 def open_session(num_sessions):
-    # Add 1 to end of range as I need counter to begin at 1 for calculations
-    for session in range(1, num_sessions + 1):
-        print("Opening session {}".format(session))
+    for session in range(num_sessions):
+        print("Opening session {}".format(session + 1))
         pyautogui.click(x=host_field_coords[0], y=host_field_coords[1])
         pyautogui.typewrite(logininfo['hostname'] + '\t')
         time.sleep(0.5)
 
-        pyautogui.typewrite(logininfo['user'] + '\t')
+        pyautogui.typewrite(logininfo['user'])
         time.sleep(0.5)
 
         pyautogui.click(x=open_sess_check_coords[0], y=open_sess_check_coords[1])
         time.sleep(2)
 
-        # Have to double click for some reason
-        pyautogui.click(x=clear_fields_coords[0], y=clear_fields_coords[1])
-        pyautogui.click(x=clear_fields_coords[0], y=clear_fields_coords[1])
+        # Have to double click the 'Clear Fields' button
+        pyautogui.click(x=clr_fields_button_coords[0], y=clr_fields_button_coords[1])
+        pyautogui.click(x=clr_fields_button_coords[0], y=clr_fields_button_coords[1])
         time.sleep(0.5)
 
-        select_session(session)
+        # Focus the current session to enter the password
+        pyautogui.click(x=sess_window_coords[0], y=sess_window_coords[1])
 
         # Delay since server can be slow to establish connection, especially for 1st session
         time.sleep(2)
         pyautogui.typewrite(logininfo['password'])
         pyautogui.press('enter')
-
-        time.sleep(2)
+        time.sleep(1)
 
     print("Done opening all sessions")
 
-def select_session(sess_num):
-    # X, Y coordinates of current tab
-    tab_position = (sess_num * sess_tab_offset[0], sess_num * sess_tab_offset[1])
+def apply_settings(num_sessions):
+    tab_y = first_sess_tab_coords[1]
+    tab_x = first_sess_tab_coords[0]
+    menu_item_x = change_settings_menu_item_coords[0]
+    menu_item_y = change_settings_menu_item_coords[1]
 
-    # Select the session tab and click into the session window
-    pyautogui.click(tab_position[0], tab_position[1])
-    time.sleep(0.5)
-    pyautogui.click(tab_position[0], tab_position[1] + sess_tab_vert_offset)
-    time.sleep(0.5)
+    for session in range(num_sessions):
+        print("Changing settings for session {}".format(session + 1))
+        pyautogui.click(button='right', x=tab_x, y=tab_y)
+        time.sleep(1.5)  # The right click menu can take some time to appear
+        pyautogui.click(x=menu_item_x, y=menu_item_y)
+        time.sleep(1)
+        pyautogui.press('enter')
+        time.sleep(0.5)
+        tab_x += settings_change_x_offset
+        menu_item_x += settings_change_x_offset
+
+    print("Done changing settings for all sessions")
 
 if __name__ == "__main__":
     main()
