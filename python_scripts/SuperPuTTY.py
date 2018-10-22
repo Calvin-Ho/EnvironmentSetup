888593ca7b095a6ab12ec14cd449dfe9d651a1d6
Calvin Ho <CalvinHoEmail@gmail.com>
2018-10-22 15:06:48 -0700

    Read login info from file instead of hardcoding
    * SESSION_COUNT value can be specified after login file argument
    * Updated git difftool to use Meld on Windows

--- python_scripts/SuperPuTTY.py
+++ python_scripts/SuperPuTTY.py
@@ -1,19 +1,20 @@
-# ---------------------------------------------
-# SCRIPT DOES NOT FUNCTION WITHOUT LOGIN INFO
-# ---------------------------------------------
 import pyautogui
 import time
+import sys
 
-SESSION_COUNT = 6
+# SESSION_COUNT defaults to 5 if not otherwise specified as command line argument
+SESSION_COUNT = 5
+USER_DELAY = 5
 
 # Hard-coded coordinates for interactive program elements
 # -------------------------------------------------------------------------------
 # (Not really any way to make this better currently as pyautogui has no support
 # for in-window references [8/20/18])
 host_field_coords = (330, 73)
+# TODO: I think this could be replaced by appending newline to end of username input
 open_sess_check_coords = (1038, 74)
 clr_fields_button_coords = (1064, 74)
-first_sess_tab_coords = (89, 137)
+first_sess_tab_coords = (89, 140)
 change_settings_menu_item_coords = (200, 340)
 
 # Store these coordinates for clicking into session window to type password
@@ -22,36 +23,52 @@ sess_window_coords = (110, 240)
 settings_change_x_offset = 136
 
 # SENSITIVE LOGIN INFORMATION
-'''
-logininfo = {
-    'hostname': '',
-    'user': '',
-    'password': '',
-}
-'''
+login_info = {}
 
 def main():
-    # Allow time for user to abort program if they wish to
-    print(">>> Allowing user 3 seconds to kill script with Ctrl-C <<<")
-    time.sleep(3)
 
+    if len(sys.argv) >= 2:
+        read_login_info(sys.argv[1])
+        if len(sys.argv) == 3:
+            print("\nSetting SESSION_COUNT to {}\n".format(sys.argv[2]))
+            SESSION_COUNT = int(sys.argv[2])
+    else:
+        print("Usage: SuperPuTTY.py [login_file] [session_count]")
+        sys.exit()
+
+    # Allow time for user to abort program if they wish to
+    print(">>> Allowing user {} seconds to kill script with Ctrl-C <<<".format(USER_DELAY))
+    time.sleep(USER_DELAY)
     open_session(SESSION_COUNT)
-    apply_settings(SESSION_COUNT)
+
+def read_login_info(config_name):
+    try:
+        with open(config_name, 'r') as config_file:
+            print("\nlogin_file found as {}".format(config_name))
+            for line in config_file:
+                values = [datum.strip() for datum in line.split('=')]
+                if values[0] == 'hostname':
+                    login_info['hostname'] = values[1]
+                elif values[0] == 'user':
+                    login_info['user'] = values[1]
+                elif values[0] == 'password':
+                    login_info['password'] = values[1]
+    except FileNotFoundError:
+        print("\nUnable to read file for login info.  Exiting.")
+        sys.exit()
+
 
 def open_session(num_sessions):
     for session in range(num_sessions):
         print("Opening session {}".format(session + 1))
         pyautogui.click(x=host_field_coords[0], y=host_field_coords[1])
-        pyautogui.typewrite(logininfo['hostname'] + '\t')
-        time.sleep(0.5)
-
-        pyautogui.typewrite(logininfo['user'])
-        time.sleep(0.5)
+        pyautogui.typewrite(login_info['hostname'] + '\t')
+        pyautogui.typewrite(login_info['user'])
 
         pyautogui.click(x=open_sess_check_coords[0], y=open_sess_check_coords[1])
         time.sleep(2)
 
-        # Have to double click the 'Clear Fields' button
+        # Have to double click the 'Clear Fields' button to clear things
         pyautogui.click(x=clr_fields_button_coords[0], y=clr_fields_button_coords[1])
         pyautogui.click(x=clr_fields_button_coords[0], y=clr_fields_button_coords[1])
         time.sleep(0.5)
@@ -61,30 +78,11 @@ def open_session(num_sessions):
 
         # Delay since server can be slow to establish connection, especially for 1st session
         time.sleep(2)
-        pyautogui.typewrite(logininfo['password'])
+        pyautogui.typewrite(login_info['password'])
         pyautogui.press('enter')
         time.sleep(1)
 
     print("Done opening all sessions")
-
-def apply_settings(num_sessions):
-    tab_y = first_sess_tab_coords[1]
-    tab_x = first_sess_tab_coords[0]
-    menu_item_x = change_settings_menu_item_coords[0]
-    menu_item_y = change_settings_menu_item_coords[1]
-
-    for session in range(num_sessions):
-        print("Changing settings for session {}".format(session + 1))
-        pyautogui.click(button='right', x=tab_x, y=tab_y)
-        time.sleep(1.5)  # The right click menu can take some time to appear
-        pyautogui.click(x=menu_item_x, y=menu_item_y)
-        time.sleep(1)
-        pyautogui.press('enter')
-        time.sleep(0.5)
-        tab_x += settings_change_x_offset
-        menu_item_x += settings_change_x_offset
-
-    print("Done changing settings for all sessions")
 
 if __name__ == "__main__":
     main()
