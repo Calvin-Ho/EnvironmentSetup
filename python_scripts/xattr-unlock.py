3e1d5f9faf27be6183559856876f9b8283981b9e
Calvin-Ho <CalvinHoEmail@gmail.com>
2021-11-09 17:50:14 -0800

    Change some variable names in unlock script

--- python_scripts/xattr-unlock.py
+++ python_scripts/xattr-unlock.py
@@ -12,18 +12,14 @@ ATTRIBUTES_COMMAND = "xattr "
 APPLE_LOCK_ATTRIBUTE = "com.apple.quarantine "
 UNLOCK_COMMAND = f"{ATTRIBUTES_COMMAND} -d {APPLE_LOCK_ATTRIBUTE}"
 
-def unlock_file(entity_name: str) -> bool:
-    '''
-    Reads the filesystem entity's extended attributes, checking for the 'com.apple.quarantine'
-    locking attribute, and attempts to unlock it if it exists.
-    '''
-    # Read file's extended attributes to determine if APPLE_LOCK_ATTRIBUTE is present
+def unlock_entity(entity_name: str) -> bool:
+    # Read entity's extended attributes to determine if APPLE_LOCK_ATTRIBUTE is present
     attributes = subprocess.check_output(f"{ATTRIBUTES_COMMAND} '{entity_name}'", shell=True)
 
-    # Decode returned byte strings from stdout based on system encoding
+    # Decode all returned byte strings based on system encoding
     attributes = attributes.decode(sys.stdout.encoding)
 
-    # Unlock file if attribute is present, printing message if the atribute is not found
+    # Unlock entity if attribute is present, printing message if the attribute is not found
     if APPLE_LOCK_ATTRIBUTE.rstrip() in attributes:
         return os.system(f"{UNLOCK_COMMAND} '{entity_name}'") == 0
     else:
@@ -35,14 +31,15 @@ def main():
         print("Usage: xattr-unlock <path> ...")
         sys.exit(os.EX_USAGE)
 
-    locked_files = sys.argv[1:]
-    unlocked_files = 0
+    locked_entities = sys.argv[1:]
 
-    for f_name in locked_files:
-        if unlock_file(f_name):
-            unlocked_files += 1
+    unlocked_entities = 0
 
-    print(f"\n\t{unlocked_files} files unlocked of {len(locked_files)} given")
+    for name in locked_entities:
+        if unlock_entity(name):
+            unlocked_entities += 1
+
+    print(f"\n\t{unlocked_entities} files unlocked of {len(locked_entities)} given")
 
 if __name__ == "__main__":
     main()
